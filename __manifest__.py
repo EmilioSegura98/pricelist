55274d6c50c6b5b483fd6552aa9032644963f2e4
Emilio Segura <112990749+EmilioSegura98@users.noreply.github.com>
2024-11-06 12:00:17 -0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -5,6 +5,7 @@
     'summary': 'Restrict pricelist access for certain users',
     'description': 'Allows configuration of price lists accessible only by specific users.',
     'depends': ['sale', 'product'],
+    'license':'LGPL-3',
     'data': [
         'security/ir.model.access.csv',
         'security/user_pricelist_restrictions_security.xml',
